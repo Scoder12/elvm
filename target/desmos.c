e14c5c36c3cf2788b12ea0c68ec16d96ca037435
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-04-29 21:03:02 -0700

    WIP: Desmos init memory

--- target/desmos.c
+++ target/desmos.c
@@ -1,7 +1,11 @@
 #include <ir/ir.h>
 #include <target/util.h>
 #include <ctype.h>
+#include <stdlib.h>
 #include <string.h>
+
+#define DESMOS_MAX_ARRAY_LEN 10000
+#define DESMOS_NUM_MEMCHUNKS 100
 
 void desmos_init_graph() {
   // getState() has a graph key with viewport info, and a randomSeed, but hese fields
@@ -9,8 +13,8 @@ void desmos_init_graph() {
   fputs("{\"version\":8,\"expressions\":{\"list\":[", stdout);
 }
 
-void desmos_emit_id(char *s) {
-  if (strlen(s) < 2) {
+void desmos_emit_id(char *s, size_t l) {
+  if (l <= 1) {
     putchar(s[0]);
   } else {
     printf("%c_{%s}", s[0], s + 1);
@@ -30,7 +34,7 @@ int desmos_get_utf8_char_len(unsigned char ch) {
   }
 }
 
-void desmos_emit_escaped_string(char *p, size_t len) {
+void desmos_emit_json_escaped_string(char *p, size_t len) {
   int cl = 0;
   const char *hex_digits = "0123456789abcdef";
   const char *specials = "btnvfr";
@@ -56,30 +60,75 @@ void desmos_emit_escaped_string(char *p, size_t len) {
   }
 }
 
-void desmos_emit_expression(int exp_id, char *latex, size_t len) {
+void desmos_start_expression(int *exp_id) {
   // getState() returns color as well but that is optional
   fputs("{\"type\":\"expression\",\"id\":", stdout);
-  printf("%d,\"latex\":\"", exp_id);
-  desmos_emit_escaped_string(latex, len);
+  *exp_id++;
+  printf("%d,\"latex\":\"", *exp_id);
+}
+
+void desmos_end_expression() {
   fputs("\"}", stdout);
+}
+
+void desmos_start_list() {
+  fputs("\\\\left[", stdout);
+}
+
+void desmos_end_list() {
+  fputs("\\\\right]", stdout);
 }
 
 void desmos_end_graph() {
   fputs("]}}", stdout);
 }
 
-void init_mem(Data *data) {
-  for (int mp = 0; data; data = data->next, mp++) {
-    if (data->v) {
-      emit_line("mem[%d] = %d;", mp, data->v);
+void desmos_start_memchunk(int *exp_id, int memchunk) {
+  desmos_start_expression(exp_id);
+  printf("m_{em%d}=", memchunk);
+  desmos_start_list();
+}
+
+void desmos_end_memchunk() {
+  desmos_end_list();
+  desmos_end_expression();
+}
+
+int desmos_init_mem(int *exp_id, Data *data) {
+  int mp, memchunk = 0;
+  desmos_start_memchunk(exp_id, 0);
+
+  while (true) {
+    if (data) {
+      printf("%d", data->v);
+      data = data->next;
+    } else {
+      putchar('0');
+    }
+    mp++;
+    if (mp >= DESMOS_MAX_ARRAY_LEN) {
+      memchunk++;
+      if (memchunk >= DESMOS_NUM_MEMCHUNKS) {
+        break;
+      }
+      mp = 0;
+      desmos_end_memchunk();
+      desmos_start_memchunk(exp_id, memchunk);
+    } else {
+      putchar(',');
     }
   }
+  desmos_end_memchunk();
+  return 0;
 }
 
 void target_desmos(Module *module) {
-  //init_mem(module->data);
+  int exp_id = 0;
+  if (desmos_init_mem(&exp_id, module->data) == -1) {
+    fputs("Data overflow", stderr);
+    return;
+  };
+  return;
   desmos_init_graph();
-  char *s = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\f";
-  desmos_emit_expression(0, s, strlen(s));
   desmos_end_graph();
 }
